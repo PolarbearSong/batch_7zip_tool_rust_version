f15e38876d259ca75f606dd4886dd74475f12938
Chen-Yin WANG <chenyin.wang@ergoss.fr>
2023-06-23 15:58:11 +0200

    A robust version

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
-use std::path::Path;
+use std::fs;
+use std::path::PathBuf;
 use std::process::Command;
 
 fn main() {
-    // Retrieve the value of the ProgramFiles environment variable
     let program_files = match env::var("ProgramFiles") {
         Ok(val) => val,
         Err(_) => {
@@ -12,34 +12,64 @@ fn main() {
         }
     };
 
-    // Construct the path to the 7-Zip executable
-    let seven_zip_path = Path::new(&program_files).join("7-Zip").join("7z.exe");
+    let seven_zip_path = PathBuf::from(&program_files).join("7-Zip").join("7z.exe");
 
-    // Check if 7-Zip executable exists
     if !seven_zip_path.exists() {
         println!("7-Zip utility not found. Please ensure it is installed correctly.");
         return;
     }
 
-    // Set the compression level (0 to 9, where 0 is no compression and 9 is the highest compression)
     let compression_level = "9";
 
-    // Iterate over each argument
     for arg in env::args().skip(1) {
-        // Check if the argument exists
-        if Path::new(&arg).exists() {
-            // Argument exists as either a file or directory
-            let output = Command::new(&seven_zip_path)
-                .args(&["a", "-tzip", &format!("-mx={}", compression_level), &format!("{}.zip", arg), &arg])
-                .output()
-                .expect("Failed to execute 7-Zip.");
-
-            if !output.status.success() {
-                println!("Compression failed for argument: {}", arg);
+        if let Ok(metadata) = fs::metadata(&arg) {
+            if metadata.is_file() {
+                compress_file(&seven_zip_path, &compression_level, &arg);
+            } else if metadata.is_dir() {
+                compress_directory(&seven_zip_path, &compression_level, &arg);
+            } else {
+                println!("Invalid argument: {}", arg);
             }
         } else {
-            // Argument does not exist
             println!("Invalid argument: {}", arg);
+        }
+    }
+}
+
+fn compress_file(seven_zip_path: &PathBuf, compression_level: &str, file_path: &str) {
+    let output = Command::new(&seven_zip_path)
+        .args(&["a", "-tzip", &format!("-mx={}", compression_level), &format!("{}.zip", file_path), file_path])
+        .output();
+
+    match output {
+        Ok(output) => {
+            if output.status.success() {
+                println!("Compression succeeded for file: {}", file_path);
+            } else {
+                println!("Compression failed for file: {}", file_path);
+            }
+        }
+        Err(err) => {
+            println!("Failed to execute 7-Zip: {}", err);
+        }
+    }
+}
+
+fn compress_directory(seven_zip_path: &PathBuf, compression_level: &str, dir_path: &str) {
+    let output = Command::new(&seven_zip_path)
+        .args(&["a", "-tzip", &format!("-mx={}", compression_level), &format!("{}.zip", dir_path), dir_path])
+        .output();
+
+    match output {
+        Ok(output) => {
+            if output.status.success() {
+                println!("Compression succeeded for directory: {}", dir_path);
+            } else {
+                println!("Compression failed for directory: {}", dir_path);
+            }
+        }
+        Err(err) => {
+            println!("Failed to execute 7-Zip: {}", err);
         }
     }
 }
